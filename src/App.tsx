526424dd34d8f5595a08684852b9d8eff92a8c8d
WindRises318 <zss428683@gmail.com>
2024-08-16 20:22:07 +0800

    增加展开及禁用滑动界面

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from 'react'
+
 import { useRoutes } from 'react-router-dom'
+import { useWebApp } from '@vkruglikov/react-telegram-web-app'
 import NavBar from './component/NavBar'
 import { routes } from './routes'
 import './App.css'
 
 function App() {
   const element = useRoutes(routes)
+  const webApp = useWebApp()
+  useEffect(() => {
+    webApp?.expand()
+    webApp?.disableVerticalSwipes()
+  }, [])
   return (
     <div className=" relative aspect-auto h-screen bg-gray-800 overflow-hidden">
       <div className='absolute bottom-0 w-full z-10'>
